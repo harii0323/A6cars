f2662a4028da208757c12ce83922a6e043ee6a01
Harikrishna <karike.harikrishnaa6@gmail.com>
2025-11-04 09:22:40 +0530

    Replacing bcrypt with bcryptjs in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const bodyParser = require('body-parser');
